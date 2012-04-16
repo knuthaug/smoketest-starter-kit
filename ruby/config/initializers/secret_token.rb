993064d464655bcaa2f60a8ed2e3102f42f6d31b
Knut Haugen <knuthaug+github@gmail.com>
2012-04-16 20:49:00 +0200

    added rails app for demo purposes

---
+++ ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smoketest::Application.config.secret_token = 'fc3fc4a6f5a34cbdb3e60d98068a6805ff4355afe7cfd95d603e20d501adaf17ed45ca327ca062e076e29f10b8d8c5bf49067ca80bfec7cfd262ca2fc3f7670b'
